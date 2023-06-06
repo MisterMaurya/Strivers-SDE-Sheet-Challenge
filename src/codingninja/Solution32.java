0d2c52d193b92537983471a7544a97a3f8b62305
Pawan Maurya <pawanm1310@gmail.com>
2023-06-06 21:05:03 +0530

    Strivers-SDE-Sheet-Challenge : 32. Cycle Detection in a Singly Linked List

---
+++ src/codingninja/Solution32.java
@@ -0,0 +1,50 @@
+package codingninja;
+
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+ public int data;
+ public Node next;
+
+ Node()
+ {
+ this.data = 0;
+ this.next = null;
+ }
+
+ Node(int data)
+ {
+ this.data = data;
+ this.next = null;
+ }
+
+ Node(int data, Node next)
+ {
+ this.data = data;
+ this.next = next;
+ }
+ }
+ *****************************************************************/
+
+//  32. Cycle Detection in a Singly Linked List 
+// https://www.codingninjas.com/codestudio/problems/cycle-detection-in-a-singly-linked-list_8230683?challengeSlug=striver-sde-challenge&leftPanelTab=1
+
+public class Solution32 {
+
+    public static boolean detectCycle(Node head) {
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
